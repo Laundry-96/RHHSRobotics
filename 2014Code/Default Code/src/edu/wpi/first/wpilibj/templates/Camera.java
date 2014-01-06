958f41cbf9334c577b8a0d8473ff35d176bc02b1
Austin DeLauney <penta2100@hotmail.com>
2014-01-05 21:38:45 -0500

    Added a Camera class and DriveTrain class
    Camera class: Adds Camera methods, so it is easier to read when we have
    to write a lot of code
    DriveTrain class: Adds flexibility and can be accessed between classes
    without having to create a new drive train instance thing

---
+++ 2014Code/Default Code/src/edu/wpi/first/wpilibj/templates/Camera.java
@@ -0,0 +1,16 @@
+/*
+ * Made by Austin DeLauney on 1/5/2014
+ * This class is supposed to do the camera stuff
+ */
+
+package edu.wpi.first.wpilibj.templates;
+
+/**
+ *
+ * @author Austin
+ */
+public class Camera 
+{
+    axis
+
+}
